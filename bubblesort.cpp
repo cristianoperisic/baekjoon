83e5e581bd52f68f640967c27dbdec964b392788
cristianoperisic <loststars0522@gmail.com>
2025-06-30 05:19:29 +0900

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using std::vector;
+
+int main(void) {
+	int N,K;
+	int n = 0;
+	std::cin >> N >> K;
+
+	vector<int> myVector(N);
+
+	for (int i = 0; i < N;i++) {
+		std::cin >> myVector[i];
+	}
+
+	for (int i = 0; i < N;i++) {
+		for (int j = 0; j < N - 1 - i;j++) {
+			if (myVector[j] > myVector[j + 1]) {
+				std::swap(myVector[j], myVector[j + 1]);
+				n++;
+			}
+			if (n == K) {
+				std::cout << myVector[j] << ' ' << myVector[j + 1];
+				n++;
+			}
+		}
+	}
+	if (n < K)
+		std::cout << -1;
+
+	return 0;
+}
